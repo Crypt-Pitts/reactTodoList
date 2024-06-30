a453f6c54c57b8caedc5a063bf6efd167fb9e373
Crypt-Pitts <95093836+Crypt-Pitts@users.noreply.github.com>
2024-06-30 00:45:19 -0400

    on enter added to submit todo

--- src/components/TodoInput.jsx
+++ src/components/TodoInput.jsx
@@ -3,17 +3,17 @@ import { useState } from "react"
 export default function TodoInput(props){
     const { handleAddTodos, todoValue, setTodoValue } = props
 
-    
+    const onEnter = (event, callback) => event.key === 'Enter' && callback();
 
     return(
         <header>
-            <input value={todoValue} onChange={(e) => {
-                setTodoValue(e.target.value)
-            }} placeholder="Enter todo..." />
-            <button onClick={() => {
-                handleAddTodos(todoValue)
-                setTodoValue('')
-            }}>Add</button>
+            
+            <input value={todoValue} onChange={(e) => {setTodoValue(e.target.value)}} placeholder="Enter todo..."  
+            onKeyDown={(e) => {
+                if (e.key === "Enter") handleAddTodos(todoValue), setTodoValue(''); 
+                                }
+                                }/>
+            <button onClick={() => {handleAddTodos(todoValue); setTodoValue('')}}>Add</button>
         </header>
     )
 }
